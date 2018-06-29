5f8f2ac3bfb56af6d14a63cca9accfaf78ded153
Simon Désaulniers <sim.desaulniers@gmail.com>
2018-06-29 16:59:11 -0400

    qurlshare: Simon D. gmail address in copyright

--- qurlshare.py
+++ qurlshare.py
@@ -1,6 +1,6 @@
 # #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright © 2018 Simon Désaulniers
+# Copyright © 2018 Simon Désaulniers <sim.desaulniers@gmail.com
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
